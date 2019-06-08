f4df157a215b03324186cbd8d1250b40bd8fa4fa
Anthony Fung <Ant-f@users.noreply.github.com>
2019-06-08 14:27:29 +0100

    Initial commit

---
+++ CustomConfig/CustomConfigSection.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+
+namespace CustomConfig
+{
+    class CustomConfigSection : ConfigurationSection
+    {
+        private const string CollectionName = "propertyPairCollection";
+
+        [ConfigurationProperty(CollectionName)]
+        [ConfigurationCollection(typeof(PropertyPairCollection), AddItemName = "add")]
+        public PropertyPairCollection PropertyPairs
+        {
+            get => (PropertyPairCollection)base[CollectionName];
+        }
+    }
+
+    public class PropertyPairCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new PropertyPair();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            var col = (PropertyPair)element;
+            return col.Id;
+        }
+    }
+
+    public class PropertyPair : ConfigurationElement
+    {
+        private const string IdName = "id";
+        private const string PropertyOneName = "propertyOne";
+        private const string PropertyTwoName = "propertyTwo";
+
+        [ConfigurationProperty(IdName, IsRequired = true)]
+        public int Id
+        {
+            get => (int)this[IdName];
+            set => this[IdName] = value;
+        }
+
+        [ConfigurationProperty(PropertyOneName, IsRequired = false)]
+        public string PropertyOne
+        {
+            get => this[PropertyOneName] as string;
+            set => this[PropertyOneName] = value;
+        }
+
+        [ConfigurationProperty(PropertyTwoName, IsRequired = false)]
+        public string PropertyTwo
+        {
+            get => this[PropertyTwoName] as string;
+            set => this[PropertyTwoName] = value;
+        }
+    }
+}
---
+++ CustomConfig/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+
+namespace CustomConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var section = ConfigurationManager.GetSection("customSection") as CustomConfigSection;
+
+            foreach (var pair in section.PropertyPairs.Cast<PropertyPair>())
+            {
+                var txt = $"ID: {pair.Id}, P1: {pair.PropertyOne}, P2: {pair.PropertyTwo}";
+                Console.WriteLine(txt);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
